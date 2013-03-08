5fcd795156459f90dca238e2374f32f603623515
Mads Nielsen <inkspot84@gmail.com>
2013-03-08 16:17:25 +0100

    Change

--- src/main/java/net/praqma/jenkins/plugin/prqa/notifier/PRQANotifier.java
+++ src/main/java/net/praqma/jenkins/plugin/prqa/notifier/PRQANotifier.java
@@ -393,7 +393,7 @@ public class PRQANotifier extends Publisher {
             } else {
                 out.println("Execution error - Writing to log");
                 log.log(Level.SEVERE, "Unhandled IOException", ex);
-                myCase.printStackTrace(out);
+                ex.printStackTrace(out);
                 
             }  
             success = false;
